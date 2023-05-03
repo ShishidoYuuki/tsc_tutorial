b274fe2e6348e6d69408543ba3507dee19d01096
Shishidoyuuki <yuuki0206nano@gmail.com>
2023-05-03 17:43:00 +0900

    typescript文法確認

---
+++ hello.ts
@@ -0,0 +1,6 @@
+function hello(firstname: string){
+    console.log("Hello" +" "+ firstname)
+}
+
+let firstname: string = "shishido"
+hello(firstname)
---
+++ kakezan.ts
@@ -0,0 +1,8 @@
+for(let i=1; i<=9; i++){
+    let row = " ";
+    for(let j=1; j<=9; j++){
+      row += " " +i*j + " "
+    }
+    console.log(row);
+  }
+  
